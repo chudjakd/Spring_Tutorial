652193ef84b1493c070e7f841179cd17f6eab325
chudjakd <chudjakd@gmail.com>
2020-09-01 11:33:08 +0200

    Vytvorenie Appkontextu a triedy AppRun

--- src/main/java/sk/dejvo/springtutorial/SpringTutorialApplication.java
+++ src/main/java/sk/dejvo/springtutorial/SpringTutorialApplication.java
@@ -2,13 +2,16 @@ package sk.dejvo.springtutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringTutorialApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SpringTutorialApplication.class, args);
+		ApplicationContext kontext=SpringApplication.run(SpringTutorialApplication.class, args);
+		AppRun appRun=kontext.getBean(AppRun.class);
+		appRun.run();
 	}
 
 }

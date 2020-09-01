64ce91b4b5e1887e1f54e58e3d4c4df4b3cb1869
chudjakd <chudjakd@gmail.com>
2020-09-01 11:37:20 +0200

    Vytvorenie triedy ktora sa vykonava pri inicializacii programu

---
+++ src/main/java/sk/dejvo/springtutorial/init/DataInit.java
@@ -0,0 +1,21 @@
+package sk.dejvo.springtutorial.init;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import sk.dejvo.springtutorial.services.MovieService;
+@Component
+public class DataInit implements ApplicationListener<ContextRefreshedEvent> {
+
+    MovieService movieService;
+
+    public DataInit(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        movieService.createAndAddMovie();
+        movieService.createAndAddMovie();
+    }
+}

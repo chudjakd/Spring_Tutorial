23fe099aec6b0ed6403ccadc495c7a832de429ab
chudjakd <chudjakd@gmail.com>
2020-09-01 10:49:43 +0200

    Vytvorenie metody na vytvorenie a pridanie filmu

--- src/main/java/sk/dejvo/springtutorial/services/MovieService.java
+++ src/main/java/sk/dejvo/springtutorial/services/MovieService.java
@@ -1,4 +1,9 @@
 package sk.dejvo.springtutorial.services;
 
+import org.springframework.web.servlet.mvc.method.annotation.ModelAndViewResolverMethodReturnValueHandler;
+import sk.dejvo.springtutorial.model.Movie;
+
 public interface MovieService {
+
+    Movie createAndAddMovie();
 }

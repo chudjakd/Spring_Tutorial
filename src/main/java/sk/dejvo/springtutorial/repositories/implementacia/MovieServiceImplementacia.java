c9f6c2587715829ca86cf8bb8923e4cf5efd0c39
chudjakd <chudjakd@gmail.com>
2020-09-01 11:07:14 +0200

    Vlozenie konstruktora

--- src/main/java/sk/dejvo/springtutorial/repositories/implementacia/MovieServiceImplementacia.java
+++ src/main/java/sk/dejvo/springtutorial/repositories/implementacia/MovieServiceImplementacia.java
@@ -1,12 +1,19 @@
 package sk.dejvo.springtutorial.repositories.implementacia;
 
+import org.springframework.stereotype.Service;
 import sk.dejvo.springtutorial.model.Movie;
 import sk.dejvo.springtutorial.repositories.MovieRepository;
 import sk.dejvo.springtutorial.services.MovieService;
 
+@Service
 public class MovieServiceImplementacia implements MovieService {
 
     MovieRepository movieRepository;
+
+    public MovieServiceImplementacia(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
     @Override
     public Movie createAndAddMovie() {
 

16b2394f0262b76b301fd820bdc2fbee63afbd67
chudjakd <chudjakd@gmail.com>
2020-09-01 10:51:29 +0200

    Vytvorenie implementacie pre movie repozitor a implementovanie metod z neho

---
+++ src/main/java/sk/dejvo/springtutorial/repositories/implementacia/MovieRepositoryImplementacia.java
@@ -0,0 +1,11 @@
+package sk.dejvo.springtutorial.repositories.implementacia;
+
+import sk.dejvo.springtutorial.model.Movie;
+import sk.dejvo.springtutorial.services.MovieService;
+
+public class MovieRepositoryImplementacia implements MovieService {
+    @Override
+    public Movie createAndAddMovie() {
+        return null;
+    }
+}

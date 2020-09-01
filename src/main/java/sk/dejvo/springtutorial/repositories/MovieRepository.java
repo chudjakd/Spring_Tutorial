f0118031e2b600fe90a8f07ac28ad0b0a61b6331
chudjakd <chudjakd@gmail.com>
2020-09-01 11:00:46 +0200

    Uprava mena MovieRepository

--- src/main/java/sk/dejvo/springtutorial/repositories/MovieRepository.java
+++ src/main/java/sk/dejvo/springtutorial/repositories/MovieRepository.java
@@ -3,5 +3,5 @@ package sk.dejvo.springtutorial.repositories;
 import org.springframework.data.repository.CrudRepository;
 import sk.dejvo.springtutorial.model.Movie;
 
-public interface movieRepository extends CrudRepository<Movie,Long> {
+public interface MovieRepository extends CrudRepository<Movie,Long> {
 }

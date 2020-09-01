90e9917efd40b57a446f6feb7ddc9e13ae4fb0ea
chudjakd <chudjakd@gmail.com>
2020-09-01 11:33:34 +0200

    Odstranenie nepotrebneho importu

--- src/main/java/sk/dejvo/springtutorial/services/MovieService.java
+++ src/main/java/sk/dejvo/springtutorial/services/MovieService.java
@@ -1,6 +1,6 @@
 package sk.dejvo.springtutorial.services;
 
-import org.springframework.web.servlet.mvc.method.annotation.ModelAndViewResolverMethodReturnValueHandler;
+
 import sk.dejvo.springtutorial.model.Movie;
 
 public interface MovieService {

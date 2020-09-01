ec9a50cea9c9d6aa86cf94c3f1a3d9834ffab656
chudjakd <chudjakd@gmail.com>
2020-09-01 09:32:01 +0200

    Vytvoril som classu Director so vsetkymi udajmi

---
+++ src/main/java/sk/dejvo/springtutorial/model/Director.java
@@ -0,0 +1,17 @@
+package sk.dejvo.springtutorial.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Director {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @ManyToMany
+    Set<Movie> movies= new HashSet<>();
+}

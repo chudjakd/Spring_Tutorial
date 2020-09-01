c12009286d244de5ccc475e902257e2e9e2a2738
chudjakd <chudjakd@gmail.com>
2020-09-01 10:23:59 +0200

    Pridanie gettrov a settrov a taktiez metody equals ktora bude porovnavat na zaklade id

--- src/main/java/sk/dejvo/springtutorial/model/Director.java
+++ src/main/java/sk/dejvo/springtutorial/model/Director.java
@@ -2,6 +2,7 @@ package sk.dejvo.springtutorial.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -15,4 +16,50 @@ public class Director {
 
     @ManyToMany(mappedBy = "directors")
     Set<Movie> movies= new HashSet<>();
+
+    public Director(Long id, String name, Set<Movie> movies) {
+        this.id = id;
+        this.name = name;
+        this.movies = movies;
+    }
+
+    public Director() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Director director = (Director) o;
+        return id.equals(director.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
